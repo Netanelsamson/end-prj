9477cc069bb180c1c7a755d5581af3de835cf330
‪Netanel Samson‬‏ <119836018+Netanelsamson@users.noreply.github.com>
2025-02-16 18:13:10 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,50 +1,45 @@
-
+#!/bin/bash
 
 is_perfect_square() {
-    n=$1
-    sqrt=$(echo "sqrt($n)" | bc)
-    squared=$((sqrt * sqrt))
+    local n=$1
+    local sqrt
+    sqrt=$(echo "scale=0; sqrt($n)/1" | bc)
+    local squared=$((sqrt * sqrt))
 
-    [ $squared -eq $n ]
+    [[ $squared -eq $n ]]
 }
 
-
 is_fibonacci() {
-    num=$1
+    local num=$1
 
-   
-    option1=$((5 * num * num + 4))
-    option2=$((5 * num * num - 4))
+    local option1=$((5 * num * num + 4))
+    local option2=$((5 * num * num - 4))
 
-    if (is_perfect_square $option1) || (is_perfect_square $option2); then
+    if is_perfect_square "$option1" || is_perfect_square "$option2"; then
         echo "$num is a Fibonacci number."
 
-     
-        a=0
-        b=1
-        index=0
+        local a=0
+        local b=1
+        local index=0
 
-      
-        while [ $a -ne $num ]; do
-            temp=$a
+        while [[ $a -ne $num ]]; do
+            local temp=$a
             a=$b
             b=$((temp + b))
             index=$((index + 1))
         done
 
-        echo "$num is at index $index in the Fibonacci."
+        echo "$num is at index $index in the Fibonacci sequence."
     else
         echo "$num is not a Fibonacci number."
     fi
 }
 
-
 user_input=$1
 
-
-if [[ $user_input =~ ^[0-9]+$ ]]; then
-  
-    is_fibonacci $user_input
+# בדיקה שהקלט הוא מספר חיובי שלם
+if [[ "$user_input" =~ ^[0-9]+$ ]]; then
+    is_fibonacci "$user_input"
 else
     echo "Invalid input. Please enter a positive integer."
 fi
